b549054d52a18e0348913c3bad1c0ef1eab2c594
Hendyvelarius <h.velarius@gmail.com>
2025-02-04 16:20:27 +0700

    Added all features and basic login logout

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Login from "./components/login";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>TATANGGA</h1>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
